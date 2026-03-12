2ea96172fa6fadc994301bf26af516ab6f64f46e
Nathan Cunha <nathanmartins@outlook.com>
2026-03-12 16:30:14 -0300

    feat(python): optimize LSP auto-imports and ruff formatting on save

--- init.el
+++ init.el
@@ -112,7 +112,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(safe-local-variable-values
-   '((eval setq-local lsp-java-java-path
+   '((pyvenv-workon . ".venv")
+     (eval setq-local lsp-java-java-path
            (string-trim (shell-command-to-string "mise which java"))))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
