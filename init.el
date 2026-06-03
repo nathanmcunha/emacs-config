1286d1f4f558b71219bb80e76b1d9c21782d6506
Nathan Cunha <nathanmartins@outlook.com>
2026-06-03 08:55:16 -0300

    refactor: nix-managed init symlinks, noctalia dark theme, font updates
    - early-init.el/init.el: now home-manager symlinks into nix store
    - tools.org: agent-shell-sidebar :ensure nil (loaded via :vc)
    - ui.org: switch to doom-gruvbox with noctalia dark-mode hooks
    - ui.org: Atkinson Hyperlegible Mono + Lexend proportional font
    - ui.org: add solaire-mode, line-spacing, variable-pitch for org

--- init.el
+++ init.el
@@ -1,15 +1,1 @@
-;;; init.el --- Bootstrap for Literate Config -*- lexical-binding: t; -*-
-
-(require 'cl-lib)
-
-;; Load the imported init file, but suppress package-manager installs so
-;; Nix-provided packages never trigger Elpaca/package-vc prompts.
-(cl-letf (((symbol-function 'package-install)
-           (lambda (&rest _args) nil))
-          ((symbol-function 'package-vc-install)
-           (lambda (&rest _args) nil))
-          ((symbol-function 'package-vc-install-from-checkout)
-           (lambda (&rest _args) nil))
-          ((symbol-function 'package-vc-install-selected-packages)
-           (lambda (&rest _args) nil)))
-  (load-file "/nix/store/g9wxrllawd39kk398vriaz9cxmd0x88a-source/init.el"))
+/nix/store/qw7vvjxac4i4vbvj00nhhdahjfg4dfwv-home-manager-files/.config/emacs/init.el
